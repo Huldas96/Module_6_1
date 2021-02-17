45061e9e0f667207d681a9b8c7f101c9b2953cd6
Hulda <huldas96@gmail.com>
2021-02-17 15:57:24 +0000

    dddd

--- pages/cheeses/index.js
+++ pages/cheeses/index.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 export const getStaticProps = async () => {
 
-    const res = await fetch("http://localhost:1337/cheeses");
+    const res = await fetch(process.env.NEXT_PUBLIC_STRAPI_URL + "/cheeses/");
     const data = await res.json();
 
     return {
